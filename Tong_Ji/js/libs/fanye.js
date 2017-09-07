1308c7e06f87d8e340c6a1f86b71b6d4780301a2
ZQ <850307035@qq.com>
2017-09-07 14:54:05 +0800

    网评结果查询filter大部分

---
+++ Tong_Ji/js/libs/fanye.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+export default class Fanye extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  create_popup_fanye() {
+    if(this.props.TP.total===0) {
+      return <div style={{height:'21px',padding: '30px 0'}}></div>;
+    }
+
+    let nums=[];
+    let start=1;
+    let end=this.props.TP.pages||1;
+    let now=this.props.TP.page||1;
+    let page_on={color:"#007A51"};
+
+    let change_page=(p)=>{
+      if(p===now) {
+        nums.push(<li key={p} style={page_on}>{p}</li>);
+      } else {
+        nums.push(<li key={p} onClick={this.fanye.bind(this,p)}>{p}</li>);
+      }
+    }
+
+    if(end<1) {
+      nums.push(<li key="only" onClick={this.fanye.bind(this,1)}>1</li>)
+    } else if(end<=5) {
+      for(let i=1;i<=end;i++) {
+        change_page(i)
+      }
+    } else {
+      if(now<3) {
+        for(let i=1;i<=5;i++) {
+        change_page(i)
+        }
+      } else if(now>end-3) {
+        for(let i=end-4;i<=end;i++) {
+        change_page(i)
+        }
+      } else {
+        for(let i=now-2;i<=now+2;i++) {
+        change_page(i)
+        }
+      }
+    }
+
+    return (<div id="fanye">
+      <span id="total">共{this.props.TP.total>=0?this.props.TP.total:1}条记录</span>
+      <input className="fanye_options" type="button" value="首页"   id="fanye_start" onClick={this.fanye.bind(this,now===1?0:1)} />
+      <input className="fanye_options" type="button" value="上一页" id="fanye_pre"   onClick={this.fanye.bind(this,now===1?0:now-1)} />
+      <ul id="fanye_nums">{nums}</ul>
+      <input type="text" id="tp" ref="tp" placeholder={`${this.props.TP.page}/${this.props.TP.pages}`} />
+      <input className="fanye_options" type="button" value="下一页" id="fanye_next"  onClick={this.fanye.bind(this,now===end?0:now+1)} />
+      <input className="fanye_options" type="button" value="尾页"   id="fanye_end"   onClick={this.fanye.bind(this,now===end?0:end)} />
+    </div>);
+  }
+
+  fanye(p) {
+    this.refs.tp.value=null;
+    if(p==0) {
+      return;
+    }
+    this.props.callback(p);
+  }
+
+  render() {
+    return this.create_popup_fanye();
+  }
+
+  componentDidMount() {
+    // 手动跳转翻页
+    this.refs.tp.onkeydown=(eve)=>{
+      if(eve.keyCode===13) {
+        let newpage=+eve.target.value;
+        if(!isNaN(newpage)) {
+          if(newpage>=1&&newpage<=this.props.TP.pages) {
+            this.fanye(newpage);
+          } else {
+            eve.target.value=null;
+          }
+        } else {
+          eve.target.value=null;
+        }
+        eve.target.blur();
+      }
+    }
+  }
+}
--- Tong_Ji/js/libs/wpjgcx.js
+++ Tong_Ji/js/libs/wpjgcx.js
@@ -1,15 +1,161 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const ajax=require('../libs/post_ajax.js');
+const Fanye=require('../libs/fanye.js');
+
+const _COUNT = 10;
+
+const SET = (key, value) => {
+  sessionStorage.setItem("wpjgcx-"+key, value);
+  return value;
+}
+
+const GET = (key) => {
+  return sessionStorage.getItem("wpjgcx-"+key) || '';
+}
+
 class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      kcmc: '',
+      wppc: '',
+      kkxy: '',
+      kkxbzx: ''
+    };
+  }
+
+  get_wppc_select() {
+    ajax({
+      url: courseCenter.host+'reviewBriefList',
+      data:{
+        userID: getCookie('userId'),
+        state: 4,
+        expGroup: ''
+      },
+      success: (gets)=>{
+        let datas=JSON.parse(gets);
+        console.log(datas);
+        let ops=[`<option value="">请选择</option>`];
+        datas.data.list.forEach(e=>{
+          ops.push(`<option value="${e.wppc}">${e.wppc}</option>`)
+        })
+        this.wppc_select.innerHTML=ops.join('');
+      }
+    })
+  }
+
+  get_kkxy_select() {
+    ajax({
+      url: courseCenter.host+'getCollege',
+      data:{
+        unifyCode: getCookie('userId')
+      },
+      success: (gets)=>{
+        let datas=JSON.parse(gets);
+        console.log(datas);
+        let ops=[`<option value="">请选择</option>`];
+        datas.data.forEach(e=>{
+          ops.push(`<option value="${e.kkxymc}">${e.kkxymc}</option>`)
+        })
+        this.kkxy_select.innerHTML=ops.join('');
+      }
+    })
+  }
+
+  get_kkxbzx_select(college) {
+    ajax({
+      url: courseCenter.host+'getCourseDepartment',
+      data:{
+        unifyCode: getCookie('userId'),
+        college: college
+      },
+      success: (gets)=>{
+        let datas=JSON.parse(gets);
+        console.log(datas);
+        let ops=[`<option value="">请选择</option>`];
+        datas.data.forEach(e=>{
+          ops.push(`<option value="${e.jysmc}">${e.jysmc}</option>`)
+        })
+        this.kkxbzx_select.innerHTML=ops.join('');
+      }
+    })
+  }
+
+  render() {
+    return (<div className='filters'>
+      <div className="top">
+        <span>网评批次：</span>
+        <select name="wppc" id="wppc" onChange={(eve)=>{this.state.wppc=eve.target.value}} ref={sel=>this.wppc_select=sel}>
+          <option value="">请选择</option>
+        </select>
+        <span>开课学院：</span>
+        <select name="kkxy" id="kkxy" onChange={(eve)=>{this.state.kkxy=eve.target.value;this.get_kkxbzx_select(eve.target.value)}} ref={sel=>this.kkxy_select=sel}>
+          <option value="">请选择</option>
+        </select>
+        <span>开课系部中心：</span>
+        <select name="kkxbzx" id="kkxbzx" onChange={(eve)=>{this.state.kkxbzx=eve.target.value}} ref={sel=>this.kkxbzx_select=sel}>
+          <option value="">请选择</option>
+        </select>
+        <span>课程名称：</span>
+        <input placeholder="请输入课程名称" type="text" value={this.state.kcmc} onChange={(eve)=>{this.setState({kcmc: eve.target.value})}}/>
+        <button id="search">搜索</button>
+      </div>
+      <Table></Table>
+    </div>);
+  }
+
+  componentDidMount() {
+    this.get_wppc_select();
+    this.get_kkxy_select();
+
+  }
+}
+
+class Table extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  create_tbody() {
+    return(<tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td>网评批次</td>
+        <td>A256231</td>
+        <td>数据库原理</td>
+        <td>专家评价</td>
+        <td>95</td>
+        <td>展开</td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>);
+  }
+
   render() {
-    return (<div>BluMUI_Filter</div>);
+    return (<table>
+      <thead>
+        <tr>
+          <td className="lefttd"><div></div></td>
+          <td width="5px"></td>
+          <td width="15%">网评批次</td>
+          <td width="15%">课程编号</td>
+          <td width="">课程名称</td>
+          <td width="15%">网评类别</td>
+          <td width="5%">得分</td>
+          <td width="10%">评价详情</td>
+          <td width="5px"></td>
+          <td className="righttd"><div></div></td>
+        </tr>
+      </thead>
+      {this.create_tbody()}
+    </table>);
   }
 }
+
 
 var BluMUI_M = {
   BluMUI_Filter: Filter
